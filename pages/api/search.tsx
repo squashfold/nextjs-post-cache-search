60bd72870c7d61d2f313ba498628770890ec6ca6
Charlie <cprincewebdev@gmail.com>
2023-06-07 23:16:32 +0100

    Add fuzzy search

--- pages/api/search.tsx
+++ pages/api/search.tsx
@@ -1,11 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type Post from '../../interfaces/post'
+import Fuse from 'fuse.js'
 
 const posts = require('../../cache/data/posts').data
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+  const fuse = new Fuse(posts, {keys: ['title', 'excerpt']})
   const query = req.query.query ? req.query.query.toString().toLowerCase() : ''
-  const results: Post[] = query.length ? posts.filter(post => post.title.toLowerCase().includes(req.query.query)) : posts
+  const results: Post[] = query.length ? fuse.search(query) : posts
 
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
