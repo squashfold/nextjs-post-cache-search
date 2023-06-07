2c2c1b76347a6e52659a22e71367ba4fd8e6645c
Charlie <cprincewebdev@gmail.com>
2023-06-07 21:44:26 +0100

    Inital setup + caching script

---
+++ cache/posts.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const fs = require('fs')
+const greyMatter = require('gray-matter')
+
+function getPostData() {
+  const postsDirectory = path.join(process.cwd(), '_posts') 
+  const fileNames = fs.readdirSync(postsDirectory)
+  const posts = fileNames.map((fileName) => {
+    const postId = fileName.replace(/\.md$/, '')
+    const fullPath = path.join(postsDirectory, fileName)
+    const postData = fs.readFileSync(fullPath, 'utf8')
+    const result = greyMatter(postData)
+    return {
+      postId,
+      slug: postId,
+      title: result.data.title,
+      thumbnail: result.data.coverImage,
+      excerpt: result.data.excerpt,
+      date: result.data.date
+    }
+  })
+
+  const postsString = JSON.stringify(posts)
+
+  return postsString
+}
+
+try {
+  fs.readdirSync('cache')
+} catch (e) {
+  fs.mkdirSync('cache')
+}
+
+
+fs.mkdir(path.join(__dirname, 'data'),
+{ recursive: true }, (error) => {
+  if (error) {
+    return console.error(error)
+  }
+
+  console.log('Directory created successfully!')
+
+  fs.writeFile(`${path.join(__dirname, 'data')}/posts.js`, getPostData(), function (error) {
+    if (error) {
+      return console.log(error)
+    }  
+    
+    console.log(`Posts cached!`)
+  })
+});
