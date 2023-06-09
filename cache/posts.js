e0011f6f6cf6f7a283a1c76b77e459fba70ac2ee
Charlie <cprincewebdev@gmail.com>
2023-06-09 14:59:26 +0100

    Comment code

--- cache/posts.js
+++ cache/posts.js
@@ -1,12 +1,12 @@
 const path = require('path')
-const fs = require('fs')
-const greyMatter = require('gray-matter')
+const fs = require('fs') // Filesystem lets us create files and directories
+const greyMatter = require('gray-matter') // Parses data from posts
 
 function getPostData() {
   const postsDirectory = path.join(process.cwd(), '_posts') 
   const fileNames = fs.readdirSync(postsDirectory)
   const posts = fileNames.map((fileName) => {
-    const postId = fileName.replace(/\.md$/, '')
+    const postId = fileName.replace(/\.md$/, '') // Replace with mdx if using MarkdownX
     const fullPath = path.join(postsDirectory, fileName)
     const postData = fs.readFileSync(fullPath, 'utf8')
     const result = greyMatter(postData)
@@ -17,7 +17,7 @@ function getPostData() {
       coverImage: result.data.coverImage,
       excerpt: result.data.excerpt,
       date: result.data.date,
-      author: result.data.author
+      author: result.data.author,
     }
   })
 
@@ -32,7 +32,7 @@ try {
   fs.mkdirSync('cache')
 }
 
-
+// Create a new directory 'data' within the cache folder
 fs.mkdir(path.join(__dirname, 'data'),
 { recursive: true }, (error) => {
   if (error) {
@@ -41,6 +41,7 @@ fs.mkdir(path.join(__dirname, 'data'),
 
   console.log('Directory created successfully!')
 
+  // Write cache file
   fs.writeFile(`${path.join(__dirname, 'data')}/posts.js`, getPostData(), function (error) {
     if (error) {
       return console.log(error)
--- pages/api/search.tsx
+++ pages/api/search.tsx
@@ -5,9 +5,9 @@ import Fuse from 'fuse.js'
 const posts = require('../../cache/data/posts').data
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  const fuse = new Fuse(posts, {keys: ['title', 'excerpt']})
-  const query = req.query.query ? req.query.query.toString().toLowerCase() : ''
-  const results: Post[] = query.length ? fuse.search(query) : posts
+  const fuse = new Fuse(posts, {keys: ['title', 'excerpt']}) // Specify which fields from the cache we should check for matches against
+  const query = req.query.query ? req.query.query.toString().toLowerCase() : '' // Convert the query to lower case so matches aren't case sensitive
+  const results: Post[] = query.length ? fuse.search(query) : posts // Find posts which match, or return all of them
 
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
